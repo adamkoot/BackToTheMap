a5d86e9a6a1341c9d355a0077f3c344409ac8bd3
pablodbm <65422439+pablodbm@users.noreply.github.com>
2022-04-23 10:59:09 +0200

    changes

--- components/tools/CountryCard.js
+++ components/tools/CountryCard.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text, Image } from "react-native";
+import poland from "../../assets/poland.png";
+import sweden from "../../assets/sweden.png";
 
 export default function MapScreen(props) {
+  console.log(props.photo);
   return (
     <TouchableOpacity
       style={styles.card}
       //   onPress={this.props.country}
     >
       <View>
+        <GetPhoto photo={props.country} />
         <Text>{props.country}</Text>
       </View>
     </TouchableOpacity>
   );
 }
-
+function GetPhoto(props) {
+  if (props.photo == "Sweden") {
+    return <Image source={sweden}></Image>;
+  } else {
+    return <Image source={poland}></Image>;
+  }
+}
 const styles = StyleSheet.create({
   card: {
     flex: 1,
-    padding: 10,
+    padding: 20,
     margin: 20,
     borderRadius: 10,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
