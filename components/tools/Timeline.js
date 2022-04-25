12888c812e7152ab8a78961060ad13ccf8aad022
pablodbm <65422439+pablodbm@users.noreply.github.com>
2022-04-25 10:43:12 +0200

    Update Timeline.js

--- components/tools/Timeline.js
+++ components/tools/Timeline.js
@@ -1,27 +1,46 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image } from "react-native";
 import Timeline from "react-native-timeline-flatlist";
 import BurgerButton from "./BurgerButton";
 
+const windowWidth = Dimensions.get("window").width;
+const photo_width = windowWidth * 0.75;
+
+
 function renderDetail(rowData, sectionID, rowID) {
   let title = (
-    <TouchableOpacity
-      onPress={() => {
-        //tu po kliku w tytul na osi czasu, powinno nam pokazac stara mape czy tam nalozyc na obecna mape
-        console.log("przechodzimy do mapki i pokazujemy miasst");
-      }}
-    >
-      <Text>{rowData.title}</Text>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          //tu po kliku w tytul na osi czasu, powinno nam pokazac stara mape czy tam nalozyc na obecna mape
+          console.log("przechodzimy do mapki i pokazujemy miasst");
+        }}
+      >
+        <Image
+          source={{ uri: "https://markow.pl/coding_da_vinci/krakow1.png" }}
+          style={{
+            resizeMode: "stretch",
+            width: photo_width,
+            height: photo_width,
+          }}
+        ></Image>
+        <Text>{rowData.title}</Text>
+      </TouchableOpacity>
+    </>
   );
   var desc = null;
-  if (rowData.description) desc = <Text>{rowData.description} dupa</Text>;
+  if (rowData.description) desc = <Text>{rowData.description}</Text>;
   return (
-    <View style={{ flex: 1 }}>
-      {title}
-      {desc}
-    </View>
+    <>
+      <View style={{ flex: 1 }}>
+        {title}
+        {desc}
+      </View>
+      <TouchableOpacity style={{ textAlign: "right" }}>
+        <Text>Czytaj dalej</Text>
+      </TouchableOpacity>
+    </>
   );
 }
 
@@ -59,14 +78,23 @@ export default function Timelinee({ route, navigation }) {
 
   let year = "1232";
 
-  var ldata = [{ title: year, description: "Event 1 Description", time: year }];
+  var ldata = [
+    { title: year, description: "Event 1 Description", time: year },
+    { title: year, description: "Event 1 Description", time: year },
+    { title: year, description: "Event 1 Description", time: year },
+    { title: year, description: "Event 1 Description", time: year },
+    { title: year, description: "Event 1 Description", time: year }
+  ];
 
   return (
     <View style={styles.container}>
       <BurgerButton view={"mainScreen"} />
       <View style={styles.list}>
         <Timeline
+          circleColor='#00305B'
           data={ldata}
+          // isUsingFlatlist={false}
+          innerCircle={'dot'}
           timeStyle={{
             textAlign: "center",
             backgroundColor: "#ff9797",
@@ -87,7 +115,8 @@ const styles = StyleSheet.create({
   },
   list: {
     flex: 1,
-    top: 250,
-    left: 80,
+    top: 150,
+    marginBottom: 100
+    // left: 80,
   },
 });
